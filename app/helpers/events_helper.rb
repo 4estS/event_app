67e8347ffda1171083df3de2e2e027cf2c42df4a
Forrest Salisbury <49921503+4estS@users.noreply.github.com>
2025-08-14 10:56:26 -0400

    Added default event image, added a check for event image in events_helper, and adjusted partial

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,11 @@
 module EventsHelper
+  def event_image(event)
+    alt_text = event.title.presence || event.slug.presence || "Event image"
+
+    if event.image.attached?
+      image_tag url_for(event.image), class: "cart-event_image max-w-full h-auto", alt: alt_text
+    else
+      image_tag "default-event.jpg", class: "card-event__image max-w-full h-auto", alt: alt_text
+    end
+  end
 end
